1dc815003fe7a2508580b8644ca7b30f1f944f17
Manuel Materazzo <materazzomanuel@gmail.com>
2023-10-02 17:31:44 +0200

    removed sumologic from google service

--- src/main/java/com/steamwatcher/service/GoogleService.java
+++ src/main/java/com/steamwatcher/service/GoogleService.java
@@ -10,58 +10,44 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.GeneralSecurityException;
 import java.util.List;
 
 @Service
 @Scope("singleton")
 public class GoogleService {
 
-    private final SumoLogicService sumoLogicService;
     private static Credential CREDENTIALS;
 
     @Value("${csgotracker.m2m-auth-file:}")
     private String authFilename;
 
-    @Autowired
-    GoogleService(SumoLogicService sumoLogicService) {
-        this.sumoLogicService = sumoLogicService;
-    }
-
-    public String getToken() {
+    public String getToken() throws IOException {
 
         // se non ho credenziali loggo
         if (CREDENTIALS == null) {
             initializeCredentials();
         }
 
-        try {
-            // se il login è scaduto refresho
-            if (CREDENTIALS.getExpiresInSeconds() < 0) {
-                boolean result = CREDENTIALS.refreshToken();
-                // se non riesco a refreshare, riloggo
-                if (!result) {
-                    initializeCredentials();
-                }
+        // se il login è scaduto refresho
+        if (CREDENTIALS.getExpiresInSeconds() < 0) {
+            boolean result = CREDENTIALS.refreshToken();
+            // se non riesco a refreshare, riloggo
+            if (!result) {
+                initializeCredentials();
             }
-        } catch (IOException e) {
-            sumoLogicService.error("google", "", "Errore durante il recupero delle credenziali m2m", e);
         }
 
         return CREDENTIALS.getAccessToken();
     }
 
-    private void initializeCredentials() {
+    private void initializeCredentials() throws IOException {
         // leggo il file
-        try (InputStream in = FileReaderUtils.getInputstream(authFilename)) {
+        InputStream in = FileReaderUtils.getInputstream(authFilename);
 
-            // estraggo la lista di scope per gli spreadsheet
-            List<String> scopes = List.of("");
-            // genero le credenziali
-            CREDENTIALS = GoogleCredential.fromStream(in).createScoped(scopes);
+        // estraggo la lista di scope per gli spreadsheet
+        List<String> scopes = List.of("");
+        // genero le credenziali
+        CREDENTIALS = GoogleCredential.fromStream(in).createScoped(scopes);
 
-        } catch (IOException e) {
-            sumoLogicService.error("google", "", "Errore durante il recupero delle credenziali m2m", e);
-        }
     }
 }
